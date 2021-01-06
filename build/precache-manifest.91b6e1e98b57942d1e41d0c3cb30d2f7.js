3bddd98d34b59aacf78317d9b98b8049cd07208c
Cezary Kocinski <ckocinski7@wp.pl>
2021-01-06 15:12:05 +0100

    deploy react app

---
+++ build/precache-manifest.91b6e1e98b57942d1e41d0c3cb30d2f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78b94bbaea584a04fa3b5d3c73149ad",
+    "url": "/iDeeper/index.html"
+  },
+  {
+    "revision": "39060614b581c1e34ecc",
+    "url": "/iDeeper/static/js/2.2d80bb48.chunk.js"
+  },
+  {
+    "revision": "0cc0e98f3d27110148b1",
+    "url": "/iDeeper/static/js/main.c617afe0.chunk.js"
+  },
+  {
+    "revision": "c72d0d40550b92379bf2",
+    "url": "/iDeeper/static/js/runtime-main.0decbdaa.js"
+  }
+]);
